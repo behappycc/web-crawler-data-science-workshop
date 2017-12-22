60eed0183fa588599a62626f7d81c0410549c02c
Hubert.Lin <hubert.lin@vivotek.com>
2017-12-22 11:55:51 +0800

    Add variables_and_types

---
+++ introduction_to_python/variables_and_types/variables_and_types.py
@@ -0,0 +1,17 @@
+print('score:', 36)
+score = 36
+print('score:', score)
+print((score ** (1/2)) * 10)
+print(type(score))
+
+first_name = 'kobe'
+last_name = 'bryant'
+name = first_name + ' ' + last_name
+print(first_name)
+print(last_name)
+print(name)
+print(type(name))
+
+condition = True
+print(condition)
+print(type(condition))
